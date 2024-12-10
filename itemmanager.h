4c3109a9739a325d4b93a89bd744cccc965c58d7
blackdover <blackdover@qq.com>
2024-12-10 21:35:18 +0800

    itemmanager还原

--- itemmanager.h
+++ itemmanager.h
@@ -8,17 +8,18 @@
 class ItemManager {
 private:
     std::vector<Item> items; // 物品列表
-
+    // 保存物品到文件的方法
+    bool saveItemsToFile();
 public:
     ItemManager();
-
     //从文件加载物品
     bool loadItemsFromFile(const QString& filename);
-
     Item* getItemByName(const std::string& name);
     std::vector<long long> generateAllPrices() const;
     const std::vector<Item>& getAllItems() const;
-    void addItem(const Item& item);  //添加物品
+    bool addItem(const Item& item);  //添加物品
+    bool modifyItem(const std::string& oldName, const Item& newItem);
+    bool deleteItem(const std::string& name);
 };
 
 #endif // ITEMMANAGER_H
