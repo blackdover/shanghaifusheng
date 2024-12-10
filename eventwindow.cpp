f316dbd3cdf13b952b0718e0449c6c45ea1fba55
blackdover <blackdover@qq.com>
2024-12-10 20:44:39 +0800

    修改已知bug

--- eventwindow.cpp
+++ eventwindow.cpp
@@ -4,27 +4,14 @@
 #include <QRandomGenerator>
 #include "ui_eventwindow.h"
 
-// Constructor
 eventwindow::eventwindow(QWidget *parent)
-    : QWidget(parent)
     : QDialog(parent)
     , ui(new Ui::eventwindow)
 {
     ui->setupUi(this);
     setWindowTitle("全球新闻");
     setWindowIcon(QIcon(":/res/icon.png"));
-      
-    events = {{"拼好饭牌脆皮鸡", {5, 30}},
-              {"面向对象课本大受欢迎", {50, 100}},
-              {"面向对象课本无人问津", {50, 100}},
-              {"软硬适中华", {100, 300}},
-              {"写满的实验报告", {300, 500}},
-              {"大米电瓶车", {700, 2000}},
-              {"华为Mate120pro", {6000, 10000}},
-              {"曹县茅台", {10000, 15000}},
-              {"冯诺依曼亲签", {20000, 40000}},
-              {"特斯拉火箭牌汽车", {200000, 500000}}};
-      
+
     if (eventManager.loadEventsFromFile(":/res/event.txt"))
         events = eventManager.getAllEvents();
     else
